311fccbc6ef873f94d1a26bd2c2a507e41a5ff50
brett.cunningham <brett.cunningham@sbevision.com>
2025-02-06 08:25:09 -0500

    completed(exercise): to use theme to change the context of the app from light to dark

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@
 
 import {useState} from "react";
 import TodoList from "./components/TodoList.jsx";
+import Header from "./components/Header.jsx";
+import { ThemeProvider} from "./context/ThemeContext.jsx";
 
 const App = () => {
 
   // we need global state for todos and theme
+
 
   const [todos, setTodos] = useState([
     { id:1,  todoText: "Take out the trash", completed: false},
@@ -32,10 +35,13 @@ const App = () => {
   }
 
   return (
+    <ThemeProvider>
       <div style={{ margin: '0 auto', maxWidth: '600px' }}>
+        <Header/>
         <h2>Todo List</h2>
-        <TodoList todos={todos} toggleTodo={toggleTodo}></TodoList>
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
       </div>
+    </ThemeProvider>
   );
 };
 
